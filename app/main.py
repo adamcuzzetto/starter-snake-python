7b2ffd184e9185517e7552b979415e3ac7035d11
adamcuzzetto <adamcuzzetto@gmail.com>
2019-02-16 22:20:55 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -56,8 +56,8 @@ def move():
     
     """
     print(json.dumps(data))
-
-    directions = ['up', 'down', 'left', 'right']
+ 
+    directions = ["up", "down", "left", "right"]
     direction = random.choice(directions)
 
     return move_response(direction)
